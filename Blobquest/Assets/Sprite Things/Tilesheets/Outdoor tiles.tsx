a569219dc26f4357df33b993f833065163903eaa
bnogh123 <35744486+bnogh123@users.noreply.github.com>
2019-02-15 15:32:50 -0600

    mattheew

--- Blobquest/Assets/Sprite Things/Tilesheets/Outdoor tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="Outdoor tiles" tilewidth="16" tileheight="16" tilecount="288" columns="24">
- <image source="../Photos/sheet (1).png" width="384" height="192"/>
-</tileset>
